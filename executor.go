65353c1f55b215fd1646de2d67c21335baaa13c8
SCP002 <feedusfeedme@gmail.com>
2021-05-02 14:28:46 +0300

    Update comments

--- executor.go
+++ executor.go
@@ -12,16 +12,16 @@ import (
 
 // Options respresents options to start process
 type Options struct {
-	Command    string
-	Args       []string
-	Print      bool
-	Capture    bool
-	Wait       bool
-	Timeout    uint
-	NewConsole bool
-	Hide       bool
-	OnChar     func(c string, p *os.Process)
-	OnLine     func(l string, p *os.Process)
+	Command    string                        // Command to run
+	Args       []string                      // Command arguments
+	Print      bool                          // Print output to console?
+	Capture    bool                          // Build buffer and capture output into Result.Output?
+	Wait       bool                          // Wait for program to finish?
+	Timeout    uint                          // Time in seconds allotted for the execution of the process before it get killed
+	NewConsole bool                          // Spawn new console window on Windows?
+	Hide       bool                          // Try to hide process window on Windows?
+	OnChar     func(c string, p *os.Process) // Callback for each character from process StdOut and StdErr
+	OnLine     func(l string, p *os.Process) // Callback for each line from process StdOut and StdErr
 }
 
 // Result respresents process run result
@@ -39,6 +39,7 @@ func Start(opts Options) Result {
 	var outSb strings.Builder
 	var err error
 
+	// Create context for command (empty or with timeout)
 	ctx := context.Background()
 	var cancel context.CancelFunc
 	if opts.Timeout > 0 {
@@ -46,9 +47,10 @@ func Start(opts Options) Result {
 		defer cancel()
 	}
 
+	// Create command
 	cmd := exec.CommandContext(ctx, opts.Command, opts.Args...)
 
-	// Fix "ERROR: Input redirection is not supported, exiting the process immediately" on Windows:
+	// Fix "ERROR: Input redirection is not supported, exiting the process immediately" on Windows
 	cmd.Stdin = os.Stdin
 
 	if opts.NewConsole || opts.Hide {
@@ -56,7 +58,7 @@ func Start(opts Options) Result {
 
 		cmd.Stderr = os.Stderr
 		cmd.Stdout = os.Stdout
-	} else { // Can capture output...
+	} else { // Can capture output
 		stdoutReader, err := cmd.StdoutPipe()
 
 		if err != nil {
@@ -64,14 +66,14 @@ func Start(opts Options) Result {
 			return res
 		}
 
-		// Redirect StdErr to StdOut:
+		// Redirect StdErr to StdOut
 		cmd.Stderr = cmd.Stdout
 
 		stdoutScanner := bufio.NewScanner(stdoutReader)
 		stdoutScanner.Split(bufio.ScanRunes)
 		var lineSb strings.Builder
 
-		// Scan output:
+		// Scan output
 		go func() {
 			for stdoutScanner.Scan() {
 				char := stdoutScanner.Text()
@@ -81,16 +83,16 @@ func Start(opts Options) Result {
 				if opts.Capture {
 					outSb.WriteString(char)
 				}
-				// Char callback:
+				// Char callback
 				if opts.OnChar != nil {
 					opts.OnChar(char, cmd.Process)
 				}
-				// Build the line:
+				// Build the line
 				if opts.OnLine != nil {
 					if char != "\n" && char != "\r" {
 						lineSb.WriteString(char)
 					} else {
-						// Line callback:
+						// Line callback
 						opts.OnLine(lineSb.String(), cmd.Process)
 						lineSb.Reset()
 					}
@@ -99,12 +101,14 @@ func Start(opts Options) Result {
 		}()
 	}
 
+	// Start the command
 	err = cmd.Start()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return res
 	}
 
+	// Wait for the command to finish execution
 	if opts.Wait {
 		err = cmd.Wait()
 		if err != nil {
@@ -116,6 +120,7 @@ func Start(opts Options) Result {
 		}
 	}
 
+	// Build and return Result
 	res.ExitCode = cmd.ProcessState.ExitCode()
 	res.Output = outSb.String()
 
