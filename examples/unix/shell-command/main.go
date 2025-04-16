e5b65fddd30d78cf185cc269c46d7d894a2f06e8
SCP002 <feedusfeedme@gmail.com>
2025-04-16 14:58:54 +0300

    Add examples/unix/shell-command

---
+++ examples/unix/shell-command/main.go
@@ -0,0 +1,39 @@
+//go:build !windows
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/SCP002/executor"
+)
+
+func main() {
+	ctx := context.Background()
+	topCmd := executor.NewCommand(ctx, executor.CmdOptions{
+		Command: "bash",
+		Args:    []string{"-c", "ps", "axu"},
+	})
+
+	res, err := topCmd.Start(executor.StartOptions{
+		ScanStdout: true,
+		ScanStderr: true,
+		Wait:       true,
+		Print:      true,
+		Capture:    true,
+		OnChar:     func(c string, p *os.Process) {},
+		OnLine:     func(l string, p *os.Process) {},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(strings.Repeat("-", 50))
+	fmt.Println("\033[92mCaptured output will be displayed below:\033[0m")
+	fmt.Print(res.Output)
+	fmt.Println(strings.Repeat("-", 50))
+	fmt.Printf("\033[92mExit code: %v\033[0m\n", res.ExitCode)
+}
